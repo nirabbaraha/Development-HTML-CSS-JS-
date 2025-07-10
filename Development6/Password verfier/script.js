391e19eab6fd0c47acc9d93e46db1fcb5145689e
nirabbaraha <nirabbaraha.dev@gmail.com>
2025-07-10 21:57:48 +0545

    Day 6 of develipment

---
+++ Development6/Password verfier/script.js
@@ -0,0 +1,8 @@
+let username=prompt("Enter User name");
+let pass=prompt("Enter the password");
+
+if(username==="admin" && password==="1234"){
+    alert("LoginSucessful");    
+}else{
+    alert("Login Unsucessful")
+}
